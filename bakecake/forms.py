cf260394eff6f2f003b5e0f03ebf77a5724b44f3
kaptsov <kaptsovaa@gmail.com>
2022-04-27 20:19:39 +0400

    Initial commit

---
+++ bakecake/forms.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.forms import ModelForm, PasswordInput, CharField
+
+
+class UserForm(ModelForm):
+
+    password = CharField(widget=PasswordInput, label='Пароль')
+
+    class Meta(object):
+        model = User
+        fields = ['username', 'email', 'password']
