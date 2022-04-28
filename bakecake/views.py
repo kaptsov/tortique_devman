d35dd5d2ae956245bbb2bc6254607f54513631ef
RobinLoksli92 <gleb11tiun@mail.ru>
2022-04-28 18:50:07 +0200

    Add registration

--- bakecake/views.py
+++ bakecake/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import FormView
+from .forms import MyForm
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'public/index.html')
+
+
+def lk(request):
+    return render(request, 'public/lk.html')
+
+
+class MyRegisterFormView(FormView):
+    form_class = MyForm
+
+    success_url = 'public/lk.html'
+    template_name = 'registration/rega.html'
+
+    def form_valid(self, form):
+        form.save()
+        return super(MyRegisterFormView, self).form_valid(form)
+
+    def form_invalid(self, form):
+        return super(MyRegisterFormView, self).form_invalid(form)
--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,7 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from bakecake.views import MyRegisterFormView
+from bakecake import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('lk/', views.lk, name='lk'),
+    path('rega/', MyRegisterFormView.as_view(), name="rega"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
